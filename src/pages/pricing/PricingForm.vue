476fde03bf7866eb64cd5e0265a29ad1033fa77f
ash <ash@ashwatdd.com>
2025-05-06 05:40:36 -0500

    Add Price display

--- src/pages/pricing/PricingForm.vue
+++ src/pages/pricing/PricingForm.vue
@@ -3,6 +3,7 @@ import type { CourseType } from '@/lib/model/CourseType.ts'
 import PlanAddOnFieldset from '@/components/Pricing/PlanAddOnFieldset.vue'
 import SelectYourPlanFieldset from '@/components/Pricing/SelectYourPlanFieldset.vue'
 import TutoringHoursSlider from '@/components/Pricing/TutoringHoursSlider.vue'
+import Card from '@/components/ui/card/Card.vue'
 import priceMocks from '@/lib/prices.mock.ts'
 import PriceService from '@/lib/service/PriceService.ts'
 import { computed, ref } from 'vue'
@@ -33,24 +34,29 @@ const totalPrice = computed(() => {
 
 <template>
   <form
-    class="grid grid-cols-1 md:grid-cols-3 gap-8
+    class="grid grid-cols-1 lg:grid-cols-3 gap-8
     font-secondary"
     @submit.prevent
   >
-    <output>
-      <span>Total Price</span>
-      <strong>${{ totalPrice }}</strong>
-      <span>/month</span>
-    </output>
-    <ul>
-      <li>Course: {{ courseType }}</li>
-      <li>Tutoring hours: {{ tutoringEnabled ? tutoringHours : 0 }}</li>
-      <li>Premium Tutor: {{ premiumTutor }}</li>
-      <li>AAMC: {{ aamcContent }}</li>
-      <li>UWorld: {{ uworldContent }}</li>
-    </ul>
-
-    <section class="md:col-span-3 space-y-8">
+    <section v-if="totalPrice === 0" class="hidden lg:block mx-auto mb-auto lg:order-1">
+      <p class="font-sans text-sm text-neutral-500">
+        select an option to get started
+      </p>
+    </section>
+    <Card
+      v-if="totalPrice > 0"
+      class="mx-auto mb-auto px-4 py-2
+      sticky top-18 lg:order-1
+      bg-white border-2 shadow-lg"
+    >
+      <p>
+        calculated pricing:
+      </p>
+      <h3 class="font-sans text-xl text-bold text-center">
+        ${{ totalPrice }}
+      </h3>
+    </Card>
+    <section class="lg:col-span-2 space-y-8">
       <SelectYourPlanFieldset
         v-model="courseType"
         :course-prices="priceMocks.coursePrices"
